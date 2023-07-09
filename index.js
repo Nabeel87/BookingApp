2847880aa957fd89f52e326e21d4c128a828ac2b
Nabell87 <nabilsabir786@gmail.com>
2023-07-09 19:15:06 +0500

    mongoDB connection done!
    after establish connection with Mongo DB, request send to local host

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 import express from "express";
+// import dotenv from "./env";
+import mongoose from "mongoose";
+
+
 const app = express()
 
-app.listen(8800, ()=>{
-    console.log("Connected to backend....")
+// dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://lama:lama@cluster0.xvpa7ry.mongodb.net/?retryWrites=true&w=majority");
+        console.log("Connected to MongoDB")
+      } catch (error) {
+        throw error;
+      }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+  console.log("mongoDB disconnected...")
 })
+mongoose.connection.on("connected", ()=>{
+  console.log("mongoDB connected..")
+})
+
+
+app.get("/", (req,res)=>{
+  res.send("hello first request");
+})
+
+
+app.listen(5000, ()=>{
+    connect();
+    console.log("Connected to backend....");
+})
+
+
+// mongoose.connect("mongodb://127.0.0.1:27017/booking");
+
+// app.listen(8000,function(){
+//   console.log('Server is running.....');
+// });
