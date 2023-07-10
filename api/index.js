7f416e2eb87e651ac5834ed139d76c20e0172e6d
Nabell87 <nabilsabir786@gmail.com>
2023-07-10 11:21:42 +0500

    db api done
    route db and error handling ok!

--- api/index.js
+++ api/index.js
@@ -32,7 +32,16 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-
+app.use((err, req,res,next)=>{
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Somthing went wrong!"
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack
+  })
+});
 
 // mongoose.connection.on("connected", ()=>{
 //   console.log("mongoDB connected..")
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -9,16 +10,71 @@ router.post("/", async(req,res)=>{
     const newHotel = new Hotel(req.body)
     
     try{
-        const saveHotel = await newHotel.save()
-        res.status(200).json(saveHotel)
+        const saveHotel = await newHotel.save();
+        res.status(200).json(saveHotel);
 
     }catch(err){
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
-})
+});
+
 //UPDATE
+router.put("/:id", async(req,res)=>{
+    
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id, 
+            { $set: req.body},
+            { new: true});
+        res.status(200).json(updatedHotel);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 //DELETE
+router.delete("/:id", async(req,res)=>{
+    
+    try{
+        await Hotel.findByIdAndDelete(
+            req.params.id
+        );
+        res.status(200).json("hotel has been deleted");
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 //GET
+router.get("/:id", async(req,res)=>{
+    
+    try{
+        const hotel = await Hotel.findById(
+            req.params.id, 
+            );
+        res.status(200).json(hotel);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 //GET ALL
+router.get("/", async(req, res, next)=>{
+
+    //error funtion call for cathing errors
+    // const failed = true;
+    // if (failed)return next(createError(401, "You are not authenticated!"));
+
+    try{
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+
+    }catch(err){
+        next(err)
+    }
+});
 
 export default router
