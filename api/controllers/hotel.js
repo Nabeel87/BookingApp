f34dbb292f563c918013c0eeb35508b15316c714
Nabell87 <nabilsabir786@gmail.com>
2023-07-17 20:39:02 +0500

    search and hotel page done

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -66,7 +66,7 @@ export const getAllHotel = async (req, res, next) => {
         const hotels = await Hotel.find({
             ...others,
             cheapestPrice: { $gt: min | 1, $lt: max || 999 },
-        }).limit(req.query.limit=4);
+        }).limit(req.query.limit);
         res.status(200).json(hotels);
     } catch (err) {
         next(err);
