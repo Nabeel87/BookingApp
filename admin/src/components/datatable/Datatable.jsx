44a0ccfeca9aef880e8f5e944639d3531800fd26
Nabell87 <nabilsabir786@gmail.com>
2023-08-02 11:59:46 +0500

    data delete successful

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -1,16 +1,29 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import useFetch from "../../hooks/useFetch";
+import axios from "axios";
 
 const Datatable = () => {
 
-  const { data, loading, error } = useFetch("/users");
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const [list, setList] = useState();
 
-  const handleDelete = (id) => {
-    // setData(data.filter((item) => item.id !== id));
+  const { data, loading, error } = useFetch(`/${path}`);
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+  console.log(list);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/${path}/${id}`);
+      setList(list.filter((item) => item._id !== id));
+    } catch (err) {}
   };
 
   const actionColumn = [
@@ -26,7 +39,7 @@ const Datatable = () => {
             </Link>
             <div
               className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
+              onClick={() => handleDelete(params.row._id)}
             >
               Delete
             </div>
@@ -38,8 +51,8 @@ const Datatable = () => {
   return (
     <div className="datatable">
       <div className="datatableTitle">
-        Add New User
-        <Link to="/users/new" className="link">
+        {path}
+        <Link to={`/${path}/new`} className="link">
           Add New
         </Link>
       </div>
